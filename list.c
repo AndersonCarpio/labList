a762133deb767d8565ac61bc92d383c849054028
Alfredo Paz <apazv@unsa.edu.pe>
2024-10-10 18:36:59 -0500

    funcion main implementada

--- list.c
+++ list.c
@@ -1,24 +1,36 @@
 #include<stdlib.h>
 #include "list.h"
 int length(struct Node* head){
-  int count = 0;
-  while(head){
-    count++;
-    head = head->next;
-  }
-  return count;
+    int count = 0;
+    while(head){
+        count++;
+        head = head->next;
+    }
+    return count;
 }
 
 struct Node* buildOneTwoThree(){
-  struct Node* head = malloc(sizeof(struct Node));
-  head->data = 1;
-  head->next = malloc(sizeof(struct Node));
+    struct Node* head = malloc(sizeof(struct Node));
+    head->data = 1;
+    head->next = malloc(sizeof(struct Node));
 
-  head->next->data = 2;
-  head->next->next = malloc(sizeof(struct Node));
+    head->next->data = 2;
+    head->next->next = malloc(sizeof(struct Node));
 
-  head->next->next->data = 1;
-  head->next->next->next = 0;
+    head->next->next->data = 1;
+    head->next->next->next = 0;
 
-  return head;
+    return head;
+}
+
+int count(struct Node* head, int searchFor){
+    int counter = 0;
+    while(head){
+        int data = head->data;
+        if(data == searchFor) counter++;
+        head = head->next;
+    }
+
+    return counter;
+
 }
--- list.h
+++ list.h
@@ -1,11 +1,11 @@
 #ifndef LIST_H
 #define LIST_H
 struct Node {
-  int data;
-  struct Node* next;
+    int data;
+    struct Node* next;
 };
 
 int length(struct Node* head);
 struct Node* buildOneTwoThree();
-
+int count(struct Node* head, int searchFor);
 #endif
